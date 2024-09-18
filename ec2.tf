967045443d51d5461d7efd6a50e3db80aa0b3382
somyagitaccount <79010341+somyagitaccount@users.noreply.github.com>
2024-09-18 15:01:20 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,9 +1,9 @@
-# resource "aws_instance" "my_ec2" {
-#   ami           = "ami-0ebfd941bbafe70c6" 
-#   instance_type = "t2.micro"    
+resource "aws_instance" "my_ec2" {
+  ami           = "ami-0ebfd941bbafe70c6" 
+  instance_type = "t2.micro"    
 
-#   tags = {
-#     Name = "MyEC2Instance"
-#   }
+  tags = {
+    Name = "MyEC2Instance"
+  }
  
-# }
+}
